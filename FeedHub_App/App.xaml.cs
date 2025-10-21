0a463b6fc88a28b404e3f02b2fa6cc9aaa1c0584
Alejandro <riveraconv@gmail.com>
2025-10-21 11:08:38 +0200

    Initial commit - solution + gitignore

---
+++ FeedHub_App/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FeedHub_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ FeedHub_App/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FeedHub_App")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "FeedHub_App.Pages")]
